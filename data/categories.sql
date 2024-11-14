bcd972cd71b32d0f7b1fbc06800d28f2e32941c5
Walid Barkatou <barkatow@ensipc384.ensimag.fr>
2024-11-14 11:10:39 +0100

    cate

--- data/categories.sql
+++ data/categories.sql
@@ -1,9 +1,9 @@
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Fruit, Fruits variés tels que bananes, pommes, oranges, etc.);
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Légumes, Légumes frais et sains tels que carottes, tomates, poivrons, etc.);
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Vêtements, Vêtements pour tous les âges et saisons, y compris les articles comme les T-shirts, pantalons, vestes, etc.);
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Bijoux, Bijoux en gold, argent, et autres matériaux, comprenant bagues, colliers, bracelets, etc.);
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Jeux, Jeux de société, jeux éducatifs, jeux vidéo, et autres jouets pour tous les âges.);
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Électronique, Appareils électroniques variés, tels que les smartphones, téléviseurs, ordinateurs, etc.);
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Articles ménagers, Équipements et objets ménagers comme les appareils électroménagers, casseroles, lampes, etc.);
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Livres, Livres dans divers genres, y compris les romans, manuels scolaires, encyclopédies, etc.);
-INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Jouets, Jouets pour enfants, y compris les poupées, figurines, puzzles, et autres jeux.);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Fruit, Fruits variés tels que bananes pommes oranges etc);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Légumes, Légumes frais et sains tels que carottes tomates, poivrons etc);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Vêtements Vêtements pour tous les âges et saisons y compris les articles comme les T-shirts pantalons vestes etc);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Bijoux, Bijoux en gold argent et autres matériaux comprenant bagues colliers bracelets etc);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Jeux, Jeux de société jeux éducatifs jeux vidéo et autres jouets pour tous les âges);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Électronique, Appareils électroniques variés tels que les smartphones téléviseurs ordinateurs etc);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Articles ménagers, Équipements et objets ménagers comme les appareils électroménagers casseroles lampes etc);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Livres, Livres dans divers genres y compris les romans manuels scolaires, encyclopédies etc);
+INSERT INTO Categorie (NOMCAT, lesCRCAT) VALUES (Jouets, Jouets pour enfants y compris les poupées figurines puzzles et autres jeux);
