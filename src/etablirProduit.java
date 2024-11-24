fb02336dcac9a69bab1f6f97d281a45495ad0091
Ouadii Zine <zineo@pcserveur.ensimag.fr>
2024-11-24 13:30:58 +0100

    amelioration des prints

--- src/etablirProduit.java
+++ src/etablirProduit.java
@@ -1,69 +1,103 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class etablirProduit 
-{
-    public static void afficherTousLesProduits(Connection connection, Scanner scanner)
-    {
-        try 
-        {
+public class etablirProduit {
+    
+    public static void afficherTousLesProduits(Connection connection, Scanner scanner) {
+        try {
             Statement stmt = connection.createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM Produit");
-            while (res.next()) 
-            {
-                System.out.println(
-                    "Identifiant du produit : " + res.getString("IdProduit") 
-                    + ", Nom du produit : " +  res.getString("NomProduit")
-                    + ", Prix Revient : " + res.getString("PrixRevient") 
-                    + ", Stock : " + res.getString("Stock") 
-                    + ", Disponibilité du produit " + res.getString("DispoProduit")
-                    + ", Nom de la catégorie : " + res.getString("NomCat"));
+
+            // Afficher l'en-tête
+            String header = String.format(
+                "%-15s %-30s %-15s %-10s %-20s %-30s", 
+                "ID Produit", "Nom du Produit", "Prix Revient", "Stock", "Disponibilité", "Nom Catégorie"
+            );
+            System.out.println("-".repeat(header.length()));
+            System.out.println(header);
+            System.out.println("-".repeat(header.length()));
+
+            // Afficher les données
+            while (res.next()) {
+                String row = String.format(
+                    "%-15s %-30s %-15s %-10s %-20s %-30s",
+                    res.getString("IdProduit"),
+                    res.getString("NomProduit"),
+                    res.getString("PrixRevient"),
+                    res.getString("Stock"),
+                    res.getString("DispoProduit"),
+                    res.getString("NomCat")
+                );
+                System.out.println(row);
             }
-        } 
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void afficherProduitSpécifique(Connection connection, Scanner scanner)
-    {
+    public static void afficherProduitSpécifique(Connection connection, Scanner scanner) {
         ResultSet res = null;
-        try 
-        {
+        try {
             PreparedStatement statement = connection.prepareStatement("SELECT * FROM Produit WHERE NomProduit LIKE ?");
             System.out.print("Nom du produit : ");
-            String nomcat = scanner.nextLine();
-            statement.setString(1, "%" + nomcat + "%");
+            String nomProduit = scanner.nextLine();
+            statement.setString(1, "%" + nomProduit + "%");
             res = statement.executeQuery();
-            while (res.next()) 
-            {
-                System.out.println("Identifiant du produit : " + res.getString("IdProduit") + ", Nom du produit : " +  res.getString("NomProduit")
-                + ", Prix Revient : " + res.getString("PrixRevient") + ", Stock : " + res.getString("Stock") + ", Disponibilité du produit " + res.getString("DispoProduit")
-                + ", Nom de la catégorie : " + res.getString("NomCat"));
+
+            // Afficher l'en-tête
+            String header = String.format(
+                "%-15s %-30s %-15s %-10s %-20s %-30s", 
+                "ID Produit", "Nom du Produit", "Prix Revient", "Stock", "Disponibilité", "Nom Catégorie"
+            );
+            System.out.println("-".repeat(header.length()));
+            System.out.println(header);
+            System.out.println("-".repeat(header.length()));
+
+            // Afficher les données
+            while (res.next()) {
+                String row = String.format(
+                    "%-15s %-30s %-15s %-10s %-20s %-30s",
+                    res.getString("IdProduit"),
+                    res.getString("NomProduit"),
+                    res.getString("PrixRevient"),
+                    res.getString("Stock"),
+                    res.getString("DispoProduit"),
+                    res.getString("NomCat")
+                );
+                System.out.println(row);
             }
-        } 
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
-    public static void afficherProduitsDispnibles(Connection connection, Scanner scanner)
-    {
-        try 
-        {
+
+    public static void afficherProduitsDispnibles(Connection connection, Scanner scanner) {
+        try {
             Statement stmt = connection.createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM Produit WHERE DISPOPRODUIT = 1");
-            while (res.next()) 
-            {
-                System.out.println("Identifiant du produit : " + res.getString("IdProduit") + ", Nom du produit : " +  res.getString("NomProduit")
-                + ", Prix Revient : " + res.getString("PrixRevient") + ", Stock : " + res.getString("Stock") + ", Disponibilité du produit " + res.getString("DispoProduit")
-                + ", Nom de la catégorie : " + res.getString("NomCat"));
+
+            // Afficher l'en-tête
+            String header = String.format(
+                "%-15s %-30s %-15s %-10s %-20s %-30s", 
+                "ID Produit", "Nom du Produit", "Prix Revient", "Stock", "Disponibilité", "Nom Catégorie"
+            );
+            System.out.println(header);
+            System.out.println("-".repeat(header.length()));
+
+            // Afficher les données
+            while (res.next()) {
+                String row = String.format(
+                    "%-15s %-30s %-15s %-10s %-20s %-30s",
+                    res.getString("IdProduit"),
+                    res.getString("NomProduit"),
+                    res.getString("PrixRevient"),
+                    res.getString("Stock"),
+                    res.getString("DispoProduit"),
+                    res.getString("NomCat")
+                );
+                System.out.println(row);
             }
-        } 
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
